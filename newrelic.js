d1758928cc1815cf15ca03e44e9a1b6444887f0b
Adrien Antoine <adriantoine@gmail.com>
2015-10-02 15:31:25 +0100

    Added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['adriantoine.com'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'ae52677577fa5f595a022bda4bfaea61fbf4861d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
