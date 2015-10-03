4f528783710515f27c31966c4e0dffc0e88903fb
Adrien Antoine <adriantoine@gmail.com>
2015-10-03 12:39:01 +0100

    Implemented consistent page titles and disqus to blog posts

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,8 @@
 
 import { createPost } from 'bloql/client';
 import React, { Component } from 'react';
+import ReactDisqusThread from 'react-disqus-thread';
+import DocumentTitle from 'react-document-title';
 import moment from 'moment';
 
 if (process.env.BROWSER) {
@@ -12,15 +14,19 @@ class Post extends Component {
   render() {
     return (
       <article className="Post">
+        <DocumentTitle title={this.props.post.meta.title}/>
 
         <header className="Post-head">
           <h1 className="Post-title">{this.props.post.meta.title}</h1>
-          <div className="Post-date">{moment(this.props.post.meta.date).format('DD/MM/YYYY')}</div>
+          <div className="Post-date">{moment(new Date(this.props.post.meta.date)).format('DD/MM/YYYY')}</div>
         </header>
 
         <div className="Post-content" dangerouslySetInnerHTML={{__html: this.props.post.content}}/>
 
+        <ReactDisqusThread shortname="adriantoine" identifier={this.props.post.meta.slug} title={this.props.post.meta.title} />
+
       </article>
+
     );
   }
 
